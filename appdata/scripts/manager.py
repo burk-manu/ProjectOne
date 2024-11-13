7926b1c6c63bfd98ec6875cb3592e2293d34789e
Manuel Bürki <burk.manu.2022@ksz.edu-zg.ch>
2024-11-13 16:13:15 +0100

    OAM (manager.py) updated by using the re-module

--- appdata/scripts/manager.py
+++ appdata/scripts/manager.py
@@ -20,57 +20,31 @@ import squareroot
 import basic_operations
 
 
-# dfinign space of variables and lists
-operators = ["sqrt", "int","f", "log", "ln", "+", "-", "*", "/", "^", "%", "sin", "cos", "tan", "cot"] # Liste der verfügberen Operatoren
 
 def programm_started(): # run when the programme starts
     prepare.prepare() # preparing and running background activities
     system.get_operating_system()
 
 
-# general functions [intro, open URLs]
 def print_intro(): # prints intro
     print(colorama.Fore.BLUE + " ✦ M A S T E R   M ∀ T H ✦ ")
     print("")
 
 
-# Operatiosn Assignment Module: checks the input and forwards it to the relevant calculation module
-def operation_assignment_module(user_input, ans):
-    if "help" in user_input: # opens document with input structure, error-code meanings and other important informations
-        system.open_link_in_browser("https://eduzg-my.sharepoint.com/:f:/g/personal/burk_manu_2022_ksz_edu-zg_ch/EviqcQd93dJOv9hP0eUGdMkBBppDHHHLWhCKwl_MPkYbLg?e=vY0rQG")
-        return "opening documents"
-    elif "ans" in user_input:
-        user_input = user_input.replace("ans",str(ans))
-    log.log_entry(f"Operation Assignment Module started [Input: {user_input}]", "Operation Assignment Module")
-    for operator in operators: # the loop checks each element in the list 'operators'
-        if operator in user_input: # checks if the operator is in the input
-            log.log_entry(f"[User Input: '{user_input}' includes Operator: '{operator}']","Operation Assignment Module")
-            try:
-                if operator in ["sqrt"]:
-                    log.log_entry("Square Root Calculating Module started", "Operation Assignment Module") # Logfile entry
-                    solution = squareroot.sqare_root_calculating_module(user_input)
-                    log.log_entry(f"received response from Square Root Calculating Module [response: {solution}]", "Operation Assignment Module") # Log eintrag
-                    return solution
-                elif operator in ["int"]:
-                    log.log_entry("Integral Calculating Module started", "Operation Assignment Module") # Logfile entry
-                    solution = integral.integral_calculating_module(user_input)
-                    log.log_entry(f"received response from Integral Calculating Module [response: {solution}]", "Operation Assignment Module") # Logfile entry
-                    return solution
-                elif operator in ["f"]: # checks if input is a function
-                    log.log_entry("Function Calculating Module started", "Operation Assignment Module") # Logfile entry
-                    solution = function.function_calculating_module(user_input)
-                    log.log_entry(f"received response from Function Calculating Module [response: {solution}]", "Operation Assignment Module") # Logfile entry
-                    return solution
-                elif operator in ["log", "ln"]: # checks if input is a logarithm
-                    log.log_entry("Logarithm Calculating Module started", "Operation Assignment Module") # Logfile entry
-                    solution = logarithm.logarithm_calculation_module(user_input)
-                    log.log_entry(f"received response from Logarithm Calculating Module [response: {solution}]", "Operation Assignment Module") # Logfile entry
-                    return solution
-                elif operator in ["+", "-", "*", "/", "^", "%"]:
-                    log.log_entry("Basic Operations Module started", "Operation Assignment Module") # Logfile entry
-                    solution = basic_operations.basic_operations_module(user_input)
-                    log.log_entry(f"received response from Basic Operations Module [response: {solution}]", "Operation Assignment Module") # Logfile entry
-                    return solution
-            except ValueError:
-                return log.error("Value Error, your input is may to big to be handeld ","Operation Assignment Module","102")
+
+
+def operation_assignment_module(user_input, ans): # Operation Assignment Module tries to figure out which kind of operation the input is
+    operations = {
+        r"^help$": lambda: system.open_link_in_browser("https://eduzg-my.sharepoint.com/:f:/g/personal/burk_manu_2022_ksz_edu-zg_ch/EviqcQd93dJOv9hP0eUGdMkBBppDHHHLWhCKwl_MPkYbLg?e=vY0rQG"), # opens help document
+        r"^sqrt\(\d+\)$": lambda: squareroot.sqare_root_calculating_module(user_input), # calculates squareroot
+        r"^int\(\d+,\d+,[\w\+\-\*\/\^]+,\w\)$": lambda: integral.integral_calculating_module(user_input), # calculates an integral
+        r"^f\(\w\) = [\w\+\-\*\/\^]+$": lambda: function.function_calculating_module(user_input), # calculates Zeros of functions
+        r"^log\(\d+\, \d+\)$" or r"^ln\(\d+\)$" or r"^log10\(\d+\)$": lambda: logarithm.logarithm_calculation_module(user_input), # calculates logarithms
+        r"^[\d\+\-\*\/\%\^]+$": lambda: basic_operations.basic_operations_module(user_input) # calculates basic operations
+        }
+    
+    for key, action in operations.items():
+        if re.match(key, user_input):
+            return action()
+    
     return log.error("Invalid input: The operation could not be executed", "Operation Assignment Module", "101") # returns an error if the input has an invalid syntax
